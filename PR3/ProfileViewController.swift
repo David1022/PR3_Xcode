a1d23592099ae93149a0f91e0610f347da655a61
David Mendaño <david.mendano@movetia.com>
2019-01-12 20:33:30 +0100

    Added loading data from internal storage

--- PR3/ProfileViewController.swift
+++ PR3/ProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileViewController: UITableViewController {
+class ProfileViewController: UITableViewController, UITextFieldDelegate {
     var currentProfile: Profile?
     
     // BEGIN-UOC-2
@@ -22,6 +22,8 @@ class ProfileViewController: UITableViewController {
     
     override func viewDidLoad() {
         currentProfile = loadProfile()
+        
+        initializeOutlets()
     }
     
     // BEGIN-UOC-3
@@ -32,13 +34,14 @@ class ProfileViewController: UITableViewController {
         saveProfile()
     }
     
+    // Declared let as a global field in order to use it in saveProfile and loadProfile
+    let profileArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("profile_data.archive")
+    }()
+    
     func saveProfile() {
-        let profileArchiveURL: URL = {
-            let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            let documentDirectory = documentsDirectories.first!
-            return documentDirectory.appendingPathComponent("profile_data.archive")
-        }()
-       
         if let userCurrentProfile = currentProfile {
             if (NSKeyedArchiver.archiveRootObject(userCurrentProfile, toFile: profileArchiveURL.path)) {
                 print("Saved profile \(userCurrentProfile.name)")
@@ -48,12 +51,64 @@ class ProfileViewController: UITableViewController {
     // END-UOC-3
     
     // BEGIN-UOC-4
+    var profiles: Profile?
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        if let savedProfiles =
+            NSKeyedUnarchiver.unarchiveObject(withFile: profileArchiveURL.path) as? Profile {
+            profiles = savedProfiles
+        }
+    }
+    
     func loadProfile() -> Profile {
+        if let savedProfiles = profiles {
+            let profile = savedProfiles
+            name.text = profile.name
+            surname.text = profile.surname
+            streetAddress.text = profile.streetAddress
+            city.text = profile.city
+            occupation.text = profile.occupation
+            company.text = profile.company
+            income.text = "\(profile.income)"
+            
+            return profile
+        }
+        
         return Profile(name: "", surname: "", streetAddress: "", city: "", occupation: "", company: "", income: 0)
     }
     // END-UOC-4
     
     // BEGIN-UOC-5
+    func initializeOutlets() {
+        self.name.delegate = self
+        self.surname.delegate = self
+        self.streetAddress.delegate = self
+        self.city.delegate = self
+        self.occupation.delegate = self
+        self.company.delegate = self
+        self.income.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == name) {
+            surname.becomeFirstResponder()
+        } else if (textField == surname) {
+            streetAddress.becomeFirstResponder()
+        } else if (textField == streetAddress) {
+            city.becomeFirstResponder()
+        } else if (textField == city) {
+            occupation.becomeFirstResponder()
+        } else if (textField == occupation) {
+            company.becomeFirstResponder()
+        } else if (textField == company) {
+            income.becomeFirstResponder()
+        } else if (textField == income) {
+            self.view.endEditing(true)
+        }
+        return false
+    }
     // END-UOC-5
     
     // BEGIN-UOC-6
