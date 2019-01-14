2f05587c47dba69b152f646ac98ddf9b8e9e1a4c
david <dav_mm@hotmail.com>
2019-01-14 08:22:24 +0100

    Image storage

---
+++ PR3/ImageStorage.swift
@@ -0,0 +1,14 @@
+//
+//  ImageStorage.swift
+//  PR3
+//
+//  Created by David Mendaño Montoro on 14/01/2019.
+//  Copyright © 2019 uoc. All rights reserved.
+//
+
+import UIKit
+
+class ImageStorage {
+    let cache = NSCache<NSString, UIImage>()
+}
+
